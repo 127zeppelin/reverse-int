46f7ed6b66e98bbe14c01dc00ae55aedbf18c80c
Lz127 <127.zeppelin@gmail.com>
2023-02-28 01:17:30 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
   
+    const myArray = n.toString().split('').map(Number);
+    const reversedArray = myArray.reverse();
+  
+    let numSum = '';
+    for (let i = 0; i < reversedArray.length; i++){
+      numSum += reversedArray[i];
+    }
+    return parseInt(numSum);
 }
